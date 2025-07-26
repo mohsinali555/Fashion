aed8920fefbb18c73f4eb22c3e66f12c51eb0a3a
Mohsin Ali <m.mohsinalisubhani5@gmail.com>
2025-07-26 23:19:40 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,130 +1,126 @@
-let navbar = document.querySelector('.navbar')
+let navbar = document.querySelector(".navbar");
 
-document.querySelector('#menu-bar').onclick = () =>{
-    navbar.classList.toggle('active');
-}
+document.querySelector("#menu-bar").onclick = () => {
+  navbar.classList.toggle("active");
+};
 
-document.querySelector('#close').onclick = () =>{
-    navbar.classList.remove('active');
-}
+document.querySelector("#close").onclick = () => {
+  navbar.classList.remove("active");
+};
 
-window.onscroll = () =>{
+window.onscroll = () => {
+  navbar.classList.remove("active");
 
-    navbar.classList.remove('active');
+  if (window.scrollY > 100) {
+    document.querySelector("header").classList.add("active");
+  } else {
+    document.querySelector("header").classList.remove("active");
+  }
+};
 
-    if(window.scrollY > 100){
-        document.querySelector('header').classList.add('active');
-    }else{
-        document.querySelector('header').classList.remove('active');
-    }
+let themeToggler = document.querySelector("#theme-toggler");
 
-}
+themeToggler.onclick = () => {
+  themeToggler.classList.toggle("fa-sun");
+  if (themeToggler.classList.contains("fa-sun")) {
+    document.querySelector("body").classList.add("active");
+  } else {
+    document.querySelector("body").classList.remove("active");
+  }
+};
 
-let themeToggler = document.querySelector('#theme-toggler');
-
-themeToggler.onclick = () =>{
-    themeToggler.classList.toggle('fa-sun');
-    if(themeToggler.classList.contains('fa-sun')){
-        document.querySelector('body').classList.add('active');
-    }else{
-        document.querySelector('body').classList.remove('active');
-    }
-}
-
-document.querySelectorAll('.small-image-1').forEach(images =>{
-    images.onclick = () =>{
-        document.querySelector('.big-image-1').src = images.getAttribute('src');
-    }
+document.querySelectorAll(".small-image-1").forEach((images) => {
+  images.onclick = () => {
+    document.querySelector(".big-image-1").src = images.getAttribute("src");
+  };
 });
 
-document.querySelectorAll('.small-image-2').forEach(images =>{
-    images.onclick = () =>{
-        document.querySelector('.big-image-2').src = images.getAttribute('src');
-    }
+document.querySelectorAll(".small-image-2").forEach((images) => {
+  images.onclick = () => {
+    document.querySelector(".big-image-2").src = images.getAttribute("src");
+  };
 });
 
-document.querySelectorAll('.small-image-3').forEach(images =>{
-    images.onclick = () =>{
-        document.querySelector('.big-image-3').src = images.getAttribute('src');
-    }
+document.querySelectorAll(".small-image-3").forEach((images) => {
+  images.onclick = () => {
+    document.querySelector(".big-image-3").src = images.getAttribute("src");
+  };
 });
 
-let countDate = new Date('aug 1, 2021 00:00:00').getTime();
+let countDate = new Date("aug 1, 2021 00:00:00").getTime();
 
-function countDown(){
+function countDown() {
+  let now = new Date().getTime();
+  gap = countDate - now;
 
-    let now = new Date().getTime();
-	gap = countDate - now;
+  let seconds = 1000;
+  let minutes = seconds * 60;
+  let hours = minutes * 60;
+  let days = hours * 24;
 
-    let seconds = 1000;
-    let minutes = seconds * 60;
-    let hours = minutes * 60;
-    let days = hours * 24;
+  let d = Math.floor(gap / days);
+  let h = Math.floor((gap % days) / hours);
+  let m = Math.floor((gap % hours) / minutes);
+  let s = Math.floor((gap % minutes) / seconds);
 
-    let d = Math.floor(gap / (days));
-	let h = Math.floor((gap % (days)) / (hours));
-	let m = Math.floor((gap % (hours)) / (minutes));
-	let s = Math.floor((gap % (minutes)) / (seconds));
-
-    document.getElementById('days').innerText = d;
-    document.getElementById('hours').innerText = h;
-    document.getElementById('minutes').innerText = m;
-    document.getElementById('seconds').innerText = s;
-
+  document.getElementById("days").innerText = d;
+  document.getElementById("hours").innerText = h;
+  document.getElementById("minutes").innerText = m;
+  document.getElementById("seconds").innerText = s;
 }
 
-setInterval(function(){
-    countDown()
-},1000);
+setInterval(function () {
+  countDown();
+}, 1000);
 
-var swiper = new Swiper(".product-slider", {
-    slidesPerView: 3,
-    loop:true,
-    spaceBetween: 10,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
+var swiper = new swiper(".product-slider", {
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+    550: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        550: {
-          slidesPerView: 2,
-        },
-        800: {
-          slidesPerView: 3,
-        },
-        1000: {
-            slidesPerView: 3,
-        },
+    800: {
+      slidesPerView: 3,
     },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
 });
 
-var swiper = new Swiper(".review-slider", {
-    slidesPerView: 3,
-    loop:true,
-    spaceBetween: 10,
-    autoplay: {
-        delay: 4000,
-        disableOnInteraction: false,
+var swiper = new swiper(".review-slider", {
+  slidesPerView: 3,
+  loop: true,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
     },
-    breakpoints: {
-        0: {
-            slidesPerView: 1,
-        },
-        550: {
-          slidesPerView: 2,
-        },
-        800: {
-          slidesPerView: 3,
-        },
-        1000: {
-            slidesPerView: 3,
-        },
+    550: {
+      slidesPerView: 2,
     },
+    800: {
+      slidesPerView: 3,
+    },
+    1000: {
+      slidesPerView: 3,
+    },
+  },
 });
